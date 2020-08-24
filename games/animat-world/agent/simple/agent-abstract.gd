9cf355a25b2c43a6b4f4c65696ef036b3a1622d6
skugele <seankugele@gmail.com>
2020-08-23 22:49:00 -0500

    Adds agent mandible controller

--- games/animat-world/agent/simple/agent-abstract.gd
+++ games/animat-world/agent/simple/agent-abstract.gd
@@ -1,9 +1,28 @@
 extends KinematicBody2D
 
-export var MAX_SPEED = 1000
-export var MAX_ROTATION = 2
-export var ACCELERATION = 1500
-export var FRICTION = 3000
+export(int) var MAX_SPEED = 1000
+export(float) var MAX_ROTATION = 20
+export(float) var APERATURE_ACCELERATION = 30 # in degrees
+export(int) var ACCELERATION = 1500
+export(int) var FRICTION = 5000
+
+export var MAX_MANDIBLE_APERATURE_IN_DEGREES = 45
+
+onready var left_mandible = $Mandibles/Left
+onready var right_mandible = $Mandibles/Right
 
 # current velocity
 var velocity = Vector2.ZERO
+var mandible_aperature = 0 # in degrees
+
+func _ready():
+	left_mandible.rotation_degrees = mandible_aperature
+	right_mandible.rotation_degrees = mandible_aperature
+
+func update_mandible_aperature(change_dir, delta):
+		var value = left_mandible.rotation_degrees \
+				  + change_dir * APERATURE_ACCELERATION * delta
+
+		if value >= 0 and value <= MAX_MANDIBLE_APERATURE_IN_DEGREES:
+			left_mandible.rotation_degrees = value
+			right_mandible.rotation_degrees = -value
--- games/animat-world/agent/simple/agent-human-controlled.gd
+++ games/animat-world/agent/simple/agent-human-controlled.gd
@@ -3,29 +3,46 @@ extends "res://agent/simple/agent-abstract.gd"
 func get_input():
 	var turn = 0
 	var direction = Vector2.ZERO
+	var mandible_change_dir = 0
 
+	# forward/backward motion
 	if Input.is_action_pressed("ui_forward"):
 		direction = Vector2(0, -1).rotated(rotation)
 	elif Input.is_action_pressed("ui_backward"):
 		direction = Vector2(0, 1).rotated(rotation)
 
+	# body rotation
 	if Input.is_action_pressed("ui_turn_right"):
 		turn += 1
 	elif Input.is_action_pressed("ui_turn_left"):
 		turn -= 1
 		
-	return [direction, turn]
+	# mandible aperature
+	if Input.is_action_pressed("ui_open_mandibles"):
+		mandible_change_dir = 1
+	elif Input.is_action_pressed("ui_close_mandibles"):
+		mandible_change_dir = -1
+		
+	return [direction, turn, mandible_change_dir]
 	
 func execute_move(inputs, delta):
 	var direction = inputs[0]
 	var turn = inputs[1]
+	var mandible_change_dir = inputs[2]
 	
+	# execute forward/backward motion
 	if direction != Vector2.ZERO:
 		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 	else:
+	# apply friction
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
+	# execute body rotation
 	rotation += turn * MAX_ROTATION * delta
+	
+	# execute mandible change
+	if mandible_change_dir != 0:
+		update_mandible_aperature(mandible_change_dir, delta)
 	
 	velocity = move_and_slide(velocity)
 
